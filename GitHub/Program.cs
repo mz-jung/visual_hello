206fcc980901441f1fadec1da7e53b11784322ce
mz-jung <jung@mz.co.kr>
2016-01-27 15:16:04 +0900

    hotfix modify

--- GitHub/Program.cs
+++ GitHub/Program.cs
@@ -11,6 +11,7 @@ namespace GitHub
         static void Main(string[] args)
         {
             Console.WriteLine("hi ~!!!!");
+            Console.WriteLine("hello~~~ for hotfix");
         }
     }
 }
